7ce164465863373f111020c055c55af3b372a513
Cory Spitzer & Frank Johnson <student@epicodus.com>
2015-06-02 09:56:03 -0700

    Create addresses

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,13 @@ var Contact = function(firstName, lastName, addresses) {
 Contact.prototype.fullName = function() {
   return this.firstName + ' ' + this.lastName;
 };
+
+var Address = function(street, city, state) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
+};
+
+Address.prototype.fullAddress = function() {
+  return this.street + ', ' + this.city + ', ' + this.state;
+};
--- spec/specs.js
+++ spec/specs.js
@@ -10,4 +10,22 @@ describe('Contact', function() {
     var sherlock = new Contact("Sherlock", "Holmes");
     expect(sherlock.fullName()).to.equal("Sherlock Holmes");
   });
+
+  // it("adds an address to a contact". function() {
+  //   var sherlock = new Contact("Sherlock", "Holmes");
+  // });
+});
+
+describe('Address', function() {
+  it('creates a new address', function () {
+    var newAddress = new Address('123 main st', 'Portland', 'OR')
+    expect(newAddress.street).to.equal('123 main st');
+    expect(newAddress.city).to.equal('Portland');
+    expect(newAddress.state).to.equal('OR');
+  });
+
+  it('adds the fullAddress method to all addresses', function () {
+    var testAddress = new Address('123 main st', 'Portland', 'OR')
+    expect(testAddress.fullAddress()).to.equal("123 main st, Portland, OR")
+  });
 });
