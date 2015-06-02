18bdfb8cafb2084b4464863f5bca5dcb6b49dfe3
Cory Spitzer & Frank Johnson <student@epicodus.com>
2015-06-02 10:46:04 -0700

    Add new address input forms

--- js/scripts.js
+++ js/scripts.js
@@ -18,3 +18,24 @@ var Address = function(street, city, state) {
 Address.prototype.fullAddress = function() {
   return this.street + ', ' + this.city + ', ' + this.state;
 };
+
+$(document).ready(function () {
+  $("#add-address").click(function() {
+    $("#new-addresses").append(
+      '<div class="new-address">' +
+         '<div class="form-group">' +
+           '<label for="new-street">Street</label>' +
+           '<input type="text" class="form-control new-street">' +
+         '</div>' +
+         '<div class="form-group">' +
+           '<label for="new-city">City</label>' +
+           '<input type="text" class="form-control new-city">' +
+         '</div>' +
+         '<div class="form-group">' +
+           '<label for="new-state">State</label>' +
+           '<input type="text" class="form-control new-state">' +
+         '</div>' +
+       '</div>'
+    );
+  });
+});
