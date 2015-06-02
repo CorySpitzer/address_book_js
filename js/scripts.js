81cc106dcb6c952287e99b7ec18e554ce66d1a12
Cory Spitzer & Frank Johnson <student@epicodus.com>
2015-06-02 11:43:01 -0700

    display someone's name and address.

--- js/scripts.js
+++ js/scripts.js
@@ -38,4 +38,40 @@ $(document).ready(function () {
        '</div>'
     );
   });
+
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+
+    $(".new-address").each(function() {
+      debugger;
+      var inputtedStreet = $(this).find("input#new-street").val();
+      var inputtedCity = $(this).find("input#new-city").val();
+      var inputtedState = $(this).find("input#new-state").val();
+
+      var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState);
+      newContact.addresses.push(newAddress);
+
+    });
+
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+// debugger;
+    $(".contact").last().click(function() {
+      $("#contact-to-show").show();
+
+      $("#contact-to-show h2").text(newContact.fullName());
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+
+      $("ul#addresses").text("");
+
+      newContact.addresses.forEach(function(address) {
+        $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
+      });
+    });
+  });
 });
